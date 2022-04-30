2b34839885856d999be6d27e03d4a36ae4a81581
Snehal2207 <snehalmotisariya@gmail.com>
2022-04-30 14:43:06 -0400

    Navigation Bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,17 @@
-import './topbar.scss'
+import "./topbar.scss";
+import Menu from "../menu/Menu";
 
 export default function topbar() {
   return (
-    <div className='topbar'>
-      
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Snehal Motisariya
+          </a>
+          <Menu />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
